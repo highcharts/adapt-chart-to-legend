872b1c5d05bd780d0f2287ee19e4db95e2c2ee53
Victor Ceban <victor.ceban@dassolutions.eu>
2017-01-12 16:59:08 +0200

    fixed FF issues with chart not rendering

--- adapt-chart-to-legend.js
+++ adapt-chart-to-legend.js
@@ -11,7 +11,7 @@
   } else {
     factory(Highcharts);
   }
-} (function (Highcharts) {
+}(function (Highcharts) {
   (function (H) {
     H.wrap(H.Legend.prototype, 'render', function (proceed) {
       var chart = this.chart,
@@ -28,13 +28,11 @@
           chart.originalChartHeight = chart.chartHeight;
         }
 
-
         if (this.options.align === 'left' || this.options.align === 'right') {
           addedHeight = Math.max(
             this.group.translateY + this.legendHeight - chart.originalChartHeight,
             0
           );
-
 
           // Move the legend down
         } else if (this.options.verticalAlign === 'bottom') {
@@ -46,19 +44,24 @@
         }
 
         if (addedHeight) {
-          // Adapt chart metrics
+          //   // Adapt chart metrics
           chart.chartHeight = chart.originalChartHeight + addedHeight;
           chart.marginBottom += addedHeight;
 
-          // Set the DOM element heights
+          //   // Set the DOM element heights
           chart.container.style.height = chart.chartHeight + 'px';
           chart.renderer.boxWrapper.attr('height', chart.chartHeight); // #7
-          chart.renderer.boxWrapper.attr('viewBox', "0 0 "+ chart.renderer.box.clientWidth + " " +chart.renderer.box.clientHeight)
+
+          var boxedElement = chart.renderer.box.clientWidth ? chart.renderer.box /*IE*/ : chart.renderer.box.parentElement /*FF*/ ;
+          var viewBox = "0 0 " + boxedElement.clientWidth + " " + boxedElement.clientHeight;
+          chart.renderer.boxWrapper.attr('viewBox', viewBox);
+
         }
 
 
         this.positionCheckboxes();
       }
+
     });
 
   }(Highcharts));
